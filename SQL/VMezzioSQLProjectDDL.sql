37f2bb54826991e7c783313e8474cb49c1144cf7
Vincenzo Mezzio <48773747+CenzOh@users.noreply.github.com>
2021-05-05 12:17:34 -0400

    Add files via upload

---
+++ SQL/VMezzioSQLProjectDDL.sql
@@ -0,0 +1,54 @@
+--remove tables
+DROP TABLE player;
+DROP TABLE gamecategory;
+DROP TABLE leaderboard;
+DROP TABLE game;
+
+--remove data
+DELETE FROM player;
+DELETE FROM gamecategory;
+DELETE FROM leaderboard;
+DELETE FROM game;
+
+CREATE TABLE player (
+	player_id INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1) --always increases by 1
+	, name VARCHAR(50) NOT NULL UNIQUE
+	, location VARCHAR(50)
+	, CONSTRAINT pk_player PRIMARY KEY (player_id, name) --id and name are pk
+	);
+
+
+CREATE TABLE game (
+	game_id INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1)
+	, gamename VARCHAR(50) NOT NULL 
+	, series VARCHAR(50)
+	, yearreleased INT 
+	, platform VARCHAR(10)
+	, CONSTRAINT pk_game PRIMARY KEY (game_id, gamename)
+	);
+
+CREATE TABLE gamecategory (
+	category_id INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1)
+	, gamename_fk VARCHAR(50) NOT NULL
+	, category VARCHAR(20) NOT NULL
+	, moderators VARCHAR(50)
+	, objectives VARCHAR(50)
+	, gameplayrestrictions VARCHAR(50)
+	--, CONSTRAINT fk_gamename FOREIGN KEY (gamename_fk) REFERENCES game(gamename) --fix me
+	, CONSTRAINT pk_gamecategory PRIMARY KEY(category_id, category) 
+	);
+
+CREATE TABLE leaderboard (
+	leaderboard_id INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1)
+	, gamename_fk VARCHAR(50) NOT NULL
+	, category_fk VARCHAR(20) NOT NULL
+	, placement VARCHAR(5)
+	, playername_fk VARCHAR(50) NOT NULL
+	, time VARCHAR(11) NOT NULL
+	, platform VARCHAR(10)
+	, verified BOOLEAN
+	, publishdate VARCHAR(8)
+	, CONSTRAINT fk_player FOREIGN KEY (playername_fk) REFERENCES player(name)
+	--, CONSTRAINT fk_gamename FOREIGN KEY (gamename_fk) REFERENCES game(gamename) --fix me
+	--, CONSTRAINT fk_category FOREIGN KEY (category_fk) REFERENCES gamecategory(category)--fix me
+	);
